973e41fd93f5d845019fa4ff0d05c7fb6c7fa080
Joacopacheco96 <97853299+Joacopacheco96@users.noreply.github.com>
2023-02-09 00:31:27 +0000

    Delete credentials.py

--- db/trainedAnswers/credentials.py
+++
@@ -1,6 +0,0 @@
-credentials={
-    'apikeyGoogle':"AIzaSyCOwegRzgCV1NMvThM1g2-xRgzq-abIcVo",
-'engineID':"https://cse.google.com/cse.js?cx=6702ec96f3ac64bec",
-'key':'AIzaSyBLE7mXeZcujKV6d_GBH2yR5re9S2NKRsU'
-
-}
